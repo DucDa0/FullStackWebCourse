5fd6b140111eb80581eda32ed6b11c737e13acff
DucmaD <hieukio1159@gmail.com>
2020-05-18 20:04:10 +0700

    Object

---
+++ JAVASCRIPT_BASIC/03. Object/main.js
@@ -0,0 +1,41 @@
+// ===Lí thuyết=== //
+// 1. Các khái niệm căn bản
+// + Object được dùng đẻ mô tả một vật thông qua các thuộc tính(Property) của vật đó
+// 2. Syntax
+/**
+ * var object_name={
+ * key: value,
+ * key: value,
+ *  ....
+ * }
+ * + Với key là tên thuộc tính
+ * + value: giá trị của thuộc tính, có thể là number, string, object, function,...
+ */
+// 3. Các thao tác trên Object
+var myCat={
+    name:'mad',
+    age: 4
+}
+// - Truy xuất đến 1 thuộc tính: myCat.name hoặc myCat['name']
+// - Xóa 1 thuộc tính: delete myCat.age
+// - Thêm 1 thuộc tính: myCat['weight']=1.5;
+
+//------------------//
+
+// ===Bài tập=== //
+
+
+/**
+ * Sử dụng kiến thức đã học khai báo object person gồm các property name, age, gender. 
+ * Ví dụ:
+ *  var person = {
+ *    name: 'Pham Thinh',
+ *    age: 25,
+ *    gender: 'male'
+ *  };
+ */
+var person={
+    name: 'Đào Văn Đức',
+    age: 21,
+    gender: 'male'
+}
