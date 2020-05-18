71729645c1ba2782122895c3678ddae2262fe63b
DucmaD <hieukio1159@gmail.com>
2020-05-18 20:14:23 +0700

    Array

---
+++ JAVASCRIPT_BASIC/04. Array/main.js
@@ -0,0 +1,27 @@
+// ===Lí thuyết=== //
+/**
+ * 1. Các khái niệm căn bản
+ * + array thường được dùng để lưu trữ một tập hợp các giá trị giống nhau về kiểu.
+ * + Array-like object: hiểu là object giống như array, Array-like object là bất cứ một object nào mà có một property length
+ * có giá trị là một số nguyên không âm.
+ * Ví dụ:
+ * var myLaptop={
+ * name: 'Asus GL753VE',
+ * price: 26000000,
+ * length: 2
+ * }
+ */
+
+//------------------//
+
+// ===Bài tập=== //
+
+
+ /*
+Khai báo một array "listAnimal" chứa các object mô tả danh sách các con vật.
+Mỗi con vật có 2 thuộc tính: name và age trong đó name có kiểu string, age có kiểu number.
+ * {name}: string;
+ * {age}: number;
+*/
+
+var listAnimal=[{name: 'mít' ,age: 12},{name: 'Cà chua',age: 4}];
