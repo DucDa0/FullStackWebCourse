a1e2d12419a35ba2a5858a5a52a8da6b283ec76a
DucmaD <hieukio1159@gmail.com>
2020-05-18 21:06:17 +0700

    Operators

---
+++ JAVASCRIPT_BASIC/05. Operators/main.js
@@ -0,0 +1,28 @@
+// ===Lí thuyết=== //
+/**
+ * Lưu ý: operator là toán tử, operand là toán hạng
+ * 1. Phân loại
+ * + Arithmetic operators (Toán tử toán học): +, -, *, /, %, ++, --
+ * + Assignment operators (Toán tử gán): +=, -=,*=,/=,=
+ * + Comparison operators (Toán tử so sánh): <,>,>=,<=,===,!==
+ * + Logical operators (Toán tử logic): 
+ *  - Toán tử AND &&: Kết quả trả về true khi cả a và b đều có giá trị true, Trả về false khi một trong hai false
+ *  - Toán tử OR ||: Kết quả trả về false khi cả a và b đều có giá trị false, Trả về true khi một trong hai true
+ *  - Toán tử NOT !: rả về giá trị nghịch đảo của toán hạng.
+ * + Bitwise operators (Toán tử bit) 
+ * + String operators (Toán tử chuỗi) 
+ * + Conditional (ternary) operator (Toán tử điều kiện)
+ * + Comma operator (Toán tử dấu phẩy) 
+ * + Unary operators (Toán tử đơn phương): có 3 loại là: delete, typeof, void
+ * + Relational operators (Toán tử quan hệ) 
+ * 2. Truthy và Falsy
+ * - Có 7 giá trị falsy trong JavaScript:
+ * + false
+ * + 0
+ * + 0n
+ * + empty string('',"",``)
+ * + null
+ * + undefined
+ * + NaN
+ * - Còn lại là Falsy
+ */
