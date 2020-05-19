51709ea573971d9d87c7546a98542b2a7f2e8251
DucmaD <hieukio1159@gmail.com>
2020-05-19 20:50:19 +0700

    Callback

---
+++ JAVASCRIPT_BASIC/10. Callback/bai1.js
@@ -0,0 +1,25 @@
+// ===Lí thuyết=== //
+/**
+ * - Bất cứ function nào được truyền vào một function khác như một tham số và được gọi sau đó, thì gọi là callback function
+ * - Function sẽ được thực thi sau khi 1 function khác được thực thi xong được gọi là Callback
+ */
+
+ /**
+ * 1. Viết hàm sayHello làm nhiệm vụ log ra màn hình 'I love Coders.Tokyo'
+ * 2. Viết hàm countAndDo nhận vào tham số là 1 hàm. Hàm này log ra màn hình từ 1 đến 10 dùng vòng lặp for. Sau khi log xong, chạy hàm được truyền vào ở tham số
+ * 3. Kiểm tra 2 hàm trên bằng cách: 
+ *   countAndDo(sayHello)
+ */
+
+function sayHello() {
+    console.log('I love Coders.Tokyo');
+  }
+  
+  function countAndDo(callback) {
+    for(var i=1;i<=10;++i){
+      console.log(i);
+    }
+    callback();
+  }
+  
+  countAndDo(sayHello);
