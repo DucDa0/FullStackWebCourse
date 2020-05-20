1ac823e17581458d81f4bfc9ffd9a6f72ca93a6f
DucmaD <hieukio1159@gmail.com>
2020-05-20 15:18:10 +0700

    Sync & Async

---
+++ JAVASCRIPT_BASIC/19. Sync. vs. Async/bai1.js
@@ -0,0 +1,18 @@
+/**
+ * Sử dụng module `request` (https://www.npmjs.com/package/request) để tải dữ liệu từ đường link sau về và hiển thị ra màn hình:
+ * https://randomuser.me/api/
+ * 
+ * Sử dụng console.time và console.timeEnd để hiển thị ra thời gian tải link. Tìm hiểu về 2 hàm này tại: https://alligator.io/js/console-time-timeend/
+ * hoặc google 'console.time'
+ */
+console.time("Time this");
+
+const request = require('request');
+request('https://randomuser.me/api/', function (error, response, body) {
+  console.error('error:', error); // Print the error if one occurred
+  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+  console.log('body:', JSON.parse(body)); // Print the HTML for the Google homepage.
+});
+
+console.timeEnd("Time this");
+
---
+++ JAVASCRIPT_BASIC/19. Sync. vs. Async/bai2.js
@@ -0,0 +1,25 @@
+/**
+ * Sử dụng module `axios` để tải dữ liệu và hiển thị ra màn hình lần lượt từng đường link (xong cái này rồi mới tới cái kia):
+ * https://jsonplaceholder.typicode.com/todos/1
+ * https://jsonplaceholder.typicode.com/todos/2
+ */
+console.log('Start');
+var axios = require('axios');
+axios.get('https://jsonplaceholder.typicode.com/todos/1')
+      .then(function(response){
+        console.log(response.data);
+      })
+      .catch(function(err){
+        console.log(err.error)
+      })
+      .then(function(){
+        return axios.get('https://jsonplaceholder.typicode.com/todos/2');
+      })
+      .then(function(response){
+        console.log(response.data);
+      })
+      .catch(function(err){
+        console.log(err.error)
+      });
+
+console.log('End');
