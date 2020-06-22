fd168e85ee0852bb5160db5e330f60c4c6dca79c
DucmaD <hieukio1159@gmail.com>
2020-06-22 23:48:29 +0700

    Bcrypt

--- Express/Book project/controller/auth_controller.js
+++ Express/Book project/controller/auth_controller.js
@@ -1,5 +1,10 @@
 const db=require('../db');
+
 const md5=require('md5');
+const bcrypt = require('bcrypt');
+// const saltRounds = 10;
+// const myPlaintextPassword = 's0/\/\P4$$w0rD';
+// const someOtherPlaintextPassword = 'not_bacon';
 // Login page
 module.exports.login=(req,res)=>{
     res.render('auth/login');
@@ -7,18 +12,41 @@ module.exports.login=(req,res)=>{
 
 module.exports.postLogin=(req,res)=>{
     let user = db.get('users').find({email: req.body.email}).value();
-    if(!user){
+    if(user){
+        if(user.wrongLoginCount>=4){
+            res.render('auth/login',{
+                errors: ['Wrong username or password over 3 times, your account is blocked!']
+            });
+            db.get('users').find({id: user.id}).assign({wrongLoginCount: ++user.wrongLoginCount}).write();
+            return;
+        }
+        bcrypt.compare(req.body.pwd, user.pwd, function(error, result) {
+            if(result) {
+                res.cookie('userId', user.id);
+                res.redirect('/');
+            } else {
+                res.render('auth/login',{
+                    errors: ['Wrong username or password!']
+                });
+                db.get('users').find({id: user.id}).assign({wrongLoginCount: ++user.wrongLoginCount}).write();
+                return;
+            } 
+        });
+    }
+    else{
         res.render('auth/login',{
-            errors: ['User does not exist!']
+            errors: ['Wrong username or password!']
         });
         return;
     }
-    if(md5(req.body.pwd)!==user.pwd){
-        res.render('auth/login',{
-            errors: ['Wrong password!']
-        });
-        return;
-    }
-    res.cookie('userId', user.id);
-    res.redirect('/');
+   
+
+
+    // if(md5(req.body.pwd)!==user.pwd){
+    //     res.render('auth/login',{
+    //         errors: ['Wrong password!']
+    //     });
+    //     return;
+    // }
+   
 };
