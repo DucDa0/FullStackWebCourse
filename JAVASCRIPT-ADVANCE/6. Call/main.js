ab609e8e730d720a96cac5781f26dcf9207bb9d4
Ducmad <hieukio1159@gmail.com>
2020-07-08 09:28:57 +0700

    Enhanced Object Literal

--- JAVASCRIPT-ADVANCE/6. Call/main.js
+++ JAVASCRIPT-ADVANCE/6. Call/main.js
@@ -6,4 +6,4 @@ var infor={
     age: 21
 }
 
-greeting.call(infor);
+greeting.call(infor);// call khac voi bind o cho: bind tra ve mot ham, va bat buoc goi = (). Con call thi goi ham do, va tra ve gia tri ma ham do tra ve
---
+++ JAVASCRIPT-ADVANCE/8. Enhanced Object Literal/main.js
@@ -0,0 +1,9 @@
+const name='Duc';
+const Cat={
+    name,
+    run(){
+        console.log(`${this.name} is running`);
+    }
+}
+
+Cat.run();
