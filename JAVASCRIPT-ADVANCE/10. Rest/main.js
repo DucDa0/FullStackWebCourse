7cd12936f4047e280d56933d5284514d30a5c2ff
Ducmad <hieukio1159@gmail.com>
2020-07-09 14:05:04 +0700

    Rest

---
+++ JAVASCRIPT-ADVANCE/10. Rest/main.js
@@ -0,0 +1,16 @@
+function sum(...numbers){
+    return numbers.reduce((a,b)=>a+b);
+}
+
+console.log(sum(1,1,5,9));
+
+function join(dot,...strings){
+    return strings.join(dot);
+}
+
+console.log(join('.','a','b','c'));
+console.log((join('.','a','b','c')).split(''));
+console.log((join('.','a','b','c')).split('.'));
+console.log((join('.','a','b','c')).split());
+var str='daovanduc';
+console.log(str.split(''));
