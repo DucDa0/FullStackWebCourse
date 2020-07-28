c03a734204de73c12b9943e59d10891b17d8cb26
Ducmad <hieukio1159@gmail.com>
2020-07-28 09:56:20 +0700

    create react app

---
+++ ReactJS/03-todo-list/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <ul>
+        <li>Go to market</li>
+        <li>Buy food</li>
+        <li>Make dinner</li>
+      </ul>
+    </div>
+  );
+}
+
+export default App;
