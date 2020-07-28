ff89a455d5c4913807120996b0628afb95c856b4
Ducmad <hieukio1159@gmail.com>
2020-07-28 09:12:38 +0700

    01. React.createElement

--- JAVASCRIPT-ADVANCE/test.js
+++ JAVASCRIPT-ADVANCE/test.js
@@ -1,6 +1,11 @@
-var foo = { bar: 'pony', baz: 3, foo: 'trump' }
-var {bar: a, baz: b} = foo
-console.log(a)
-// <- 'pony'
-console.log(b)
+function Cat(name, age){
+    this.name=name;
+    this.age=age;
+    this.showInfo=function(){
+        console.log(`${this.name} is ${this.age}`);
+    }
+};
 
+const tom =new Cat('Tom',21);
+tom.proto.prototype=5;
+console.log(tom)
