39c0d75935955a37ec9378e90d4b47be3401e1b8
Ducmad <hieukio1159@gmail.com>
2020-08-04 10:21:01 +0700

    props.children

--- ReactJS/02. bai-tap-tong-hop/src/App.js
+++ ReactJS/02. bai-tap-tong-hop/src/App.js
@@ -6,8 +6,8 @@ import './css/main.css';
 // import RecommendedFriends from './components/RecommendedFriends';
 // import DynamicClassNames from './components/DynamicClassNames';
 // import Notification from './components/Notification';
-import SearchBox from './components/SearchBox'
-// import Modal from './components/Modal'
+// import SearchBox from './components/SearchBox'
+import Modal from './components/Modal'
 
 export default class App extends Component{
   constructor(){
@@ -35,8 +35,27 @@ export default class App extends Component{
         {/* <DynamicClassNames/> */}
         {/* <Notification hasUnread={true}/> */}
         {/* <SearchBox onFocused={this.isClick()} isFocused={trigger}/> */}
-        {/* <button onClick={this.isClick()}>Open modal</button>
-        <Modal onClick={this.isClick()} isTrigger={trigger}/> */}
+        <button onClick={this.isClick()}>Open modal</button>
+        {
+          trigger && <Modal onClick={this.isClick()}>
+                            <h2 className="title">This is my life</h2>
+                            <p className="article">
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                                Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+                                Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+                                Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. 
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                                Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+                                Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+                                Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. 
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                                Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+                                Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+                                Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                            </p>
+                          
+                    </Modal>
+        }
       </div>
     );
   }
--- ReactJS/02. bai-tap-tong-hop/src/components/Modal.js
+++ ReactJS/02. bai-tap-tong-hop/src/components/Modal.js
@@ -1,32 +1,20 @@
 import React, { Component } from 'react'
-import classNames from 'classnames'
+// import classNames from 'classnames'
 import '../css/Modal.css'
 export default class Modal extends Component {
     render() {
-        const {onClick, isTrigger}=this.props;
+        const {onClick,children}=this.props;
+        console.log(children)
         return (
-            <div className={classNames('Modal',{display:isTrigger})}>
+            <div className="Modal">
                 <div className="Modal-wrap">
                     <div className="Modal-content">
                         <div className="head">
-                            <h2 className="title">This is my life</h2>
+                            {children[0]}
                             <div onClick={onClick} className="close"><i className="fa fa-times" aria-hidden="true"></i></div>
                         </div>
                         <div className="body">
-                            <p className="article">
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
-                            Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
-                            Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. 
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-                            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
-                            Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
-                            Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. 
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-                            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
-                            Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-                            Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                            </p>
+                            {children[1]}
                         </div>
                         <div className="foot">
                             <button onClick={onClick} className="accept">Accept</button>
