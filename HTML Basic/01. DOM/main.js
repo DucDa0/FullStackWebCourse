6dee265ffff32f03f4afc94188c642f116022a3c
Ducmad <hieukio1159@gmail.com>
2020-11-09 23:33:57 +0700

    sleep.....

---
+++ HTML Basic/01. DOM/main.js
@@ -0,0 +1,6 @@
+const boxElement = document.getElementById('box');
+Object.assign(boxElement.style, {
+  width: '20px',
+  height: '50px',
+  backgroundColor: 'green',
+});
--- JAVASCRIPT_BASIC/clone_popular_method/index.js
+++ JAVASCRIPT_BASIC/clone_popular_method/index.js
@@ -1,18 +1,15 @@
 //* ---------filter-----------
-Array.prototype.cloneFilter=function(cb){
-    let res=[];
-    for(item of this){
-        if(cb(item)){
-            res.push(item);
-        }
-        
+Array.prototype.cloneFilter = function (cb) {
+  let res = [];
+  for (item of this) {
+    if (cb(item)) {
+      res.push(item);
     }
-    return res;
-}
+  }
+  return res;
+};
 //* ex:
-const res = [1,2,3,4,5,6].cloneFilter(function(item){
-    return item%2===0;
-})
+const res = [1, 2, 3, 4, 5, 6].cloneFilter(function (item) {
+  return item % 2 === 0;
+});
 console.log(res);
-
-//* ---------filter-----------
