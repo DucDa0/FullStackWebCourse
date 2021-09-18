ca84d280d26d80eb95239b54e0c800ae348dc711
Duc Dao <ducdv@dgroup.co>
2021-09-18 23:49:36 +0700

    .

--- JAVASCRIPT-ADVANCE/15. Hoisting/main.js
+++ JAVASCRIPT-ADVANCE/15. Hoisting/main.js
@@ -14,5 +14,26 @@ var subtract = function (num1, num2) {
 /**
  * *Hoisting là hành động mặc định của Javascript, nó sẽ chuyển phần khai báo
  * *lên phía trên top Trong Javascript, một biến (variable) có thể được khai báo sau khi được sử dụng.
- * * const, let không hỗ trợ hoisting
+ * * const, let hoisting sẽ khác vs var, phần khai báo cũng được đưa lên top và lưu vào bộ nhớ, nhưng lại đc ưa vào Temporal Dead Zone nên sẽ ko sử dụng được
  */
+
+//Lưu ý: Biến được khai báo với var & function declaration được khai báo trong code block sẽ được hoisted phần hàm trong chính block đó và hoisted tên hàm ra ngoài block.
+// Ví dụ 1:
+console.log(sum); // undefined
+
+{
+  sum(1, 2); // 3
+
+  function sum(a, b) {
+    return a + b;
+  }
+}
+
+// Ví dụ 2:
+sum(1, 2); // sum is not a function
+
+{
+  function sum(a, b) {
+    return a + b;
+  }
+}
--- Typescript/main.ts
+++ Typescript/main.ts
@@ -1,1 +1,31 @@
-let _name: string = "duc";
+class Tuong {
+  name: string;
+  type: string;
+  skills: string[];
+
+  constructor(name: string, type: string, skills: string[]) {
+    this.name = name;
+    this.type = type;
+    this.skills = skills;
+  }
+
+  getInfo(): string {
+    return `${this.name}, ${this.type}, ${this.skills}`;
+  }
+}
+
+class Miner extends Tuong {
+  dig: number;
+  constructor(name: string, type: string, skills: string[], dig: number) {
+    super(name, type, skills);
+    this.dig = dig;
+  }
+
+  getInfo(): string {
+    const baseInfo = super.getInfo();
+    return `${baseInfo}, ${this.dig}`;
+  }
+}
+
+const miner = new Miner("Miner", "Earth", ["dao mo"], 500);
+console.log(miner.getInfo());
